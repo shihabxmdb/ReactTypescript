edc9e9f5236187a8cef627c6e92948845bbf71fd
shihabxmdb <shihab.xm.db@gmail.com>
2023-10-25 09:32:58 +0100

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import { Greet } from "./components/Greet";
+import { Heading } from "./components/Heading";
 import { Status } from "./components/Status";
+import { Oscar } from "./components/Oscar";
 
 function App() {
   return (
     <div className="App">
-      <Status Status="error" />
+      <Status Status="success" />
+      <Heading>Placeholder Test</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicpario</Heading>
+      </Oscar>
+      <Greet name="Hasannnnn"  isLogedIn={true} />
     </div>
   );
 }
