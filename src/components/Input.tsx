dfb5805977729cc70c624cf77faaae5e3063ff13
shihabxmdb <shihab.xm.db@gmail.com>
2023-10-25 20:03:46 +0100

    first

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,11 @@
-type InputProps={
-    value:string;
-    handleChange:(event:React.ChangeEvent<HTMLInputElement>)=>void;
-}
+type InputProps = {
+  value: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
-export const Input = (props:InputProps) => {
-   
-  return <input type="text" value={props.value} onChange={props.handleChange}/>
-}
+export const Input = (props: InputProps) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event);
+  };
+  return <input type="text" value={props.value} onChange={handleInputChange} />;
+};
