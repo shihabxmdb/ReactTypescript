3f373e4d295de86fa14f45733b8e6e47c2a0fa64
shihabxmdb <shihab.xm.db@gmail.com>
2023-10-26 18:32:58 +0100

    video-7 firstpart

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
 import "./App.css";
-import { Button } from "./components/Button.tsx";
-import { Input } from "./components/Input.tsx";
+
 function App() {
-  return (
-    <div className="App">
-      <Button
-        handleClick={(event, id) => {
-          console.log("Button Clicked", event, id);
-        }}
-      />
-      <Input value="" handleChange={(event) => console.log(event)} />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/components/Input.tsx
+++
@@ -1,9 +0,0 @@
-type InputProps={
-    value:string;
-    handleChange:(event:React.ChangeEvent<HTMLInputElement>)=>void;
-}
-
-export const Input = (props:InputProps) => {
-   
-  return <input type="text" value={props.value} onChange={props.handleChange}/>
-}
